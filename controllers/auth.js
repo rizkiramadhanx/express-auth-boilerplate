73987b7629f73096b4ca0936d51a8c0e50f1d02d
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-01-13 14:42:57 +0700

    feat : fix response and cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
-const User = require("../models/User")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 /*
     This is registration controller.
@@ -10,19 +10,19 @@ const jwt = require("jsonwebtoken")
 */
 
 exports.register = async (req, res, next) => {
-  const { username, email, password } = req.body
+  const { username, email, password } = req.body;
 
   //   Check if already in DB
   const isAlreadyRegisterd = await User.findOne({
     $or: [{ email }, { username }],
-  })
+  });
 
   if (isAlreadyRegisterd) {
-    return res.status(400).json({
+    return res.status(422).json({
       success: false,
-      msg: "User already registered",
-      code: "ALREADY_REGISTERED",
-    })
+      msg: 'User already registered',
+      code: 'ALREADY_REGISTERED',
+    });
   }
 
   try {
@@ -31,24 +31,24 @@ exports.register = async (req, res, next) => {
       username,
       email: email.toLowerCase(),
       password,
-    })
+    });
 
     // Save him in db
-    await user.save()
+    await user.save();
 
     // send cookie
-    sendCookie(user, 200, "User registered !", "REGISTRATION_SUCCESS", res)
+    sendCookie(user, 201, 'User registered !', 'REGISTRATION_SUCCESS', res);
   } catch (error) {
-    return res.status(500).json({
+    return res.status(424).json({
       success: false,
       msg: error,
-      code: "INTERNAL_ERROR",
-    })
+      code: 'INTERNAL_ERROR',
+    });
   }
-}
+};
 
 exports.login = async (req, res, next) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
   /*
     backend validation. It's recommended that you do not remove 
     this
@@ -56,59 +56,59 @@ exports.login = async (req, res, next) => {
   if (!email || !password) {
     return res.status(400).json({
       success: false,
-      msg: "No email or password provided",
-      code: "NO_CREDENTIALS",
-    })
+      msg: 'No email or password provided',
+      code: 'NO_CREDENTIALS',
+    });
   }
   try {
-    const user = await User.findOne({ email })
+    const user = await User.findOne({ email });
 
     if (!user) {
       return res.staus(404).json({
         success: false,
-        msg: "No user with that email was found",
-        code: "USER_NOT_FOUND",
-      })
+        msg: 'No user with that email was found',
+        code: 'USER_NOT_FOUND',
+      });
     }
-    const passwordsMatch = await bcrypt.compare(password, user.password)
+    const passwordsMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordsMatch) {
       return res.status(401).json({
         success: false,
-        msg: "Incorrect Credentials",
-        code: "INVALID_CREDS",
-      })
+        msg: 'Incorrect Credentials',
+        code: 'INVALID_CREDS',
+      });
     }
 
-    sendCookie(user, 200, "Logged in successfully!", "LOGIN_SUCCESS", res)
+    sendCookie(user, 200, 'Logged in successfully!', 'LOGIN_SUCCESS', res);
   } catch (error) {
     res.status(500).json({
       success: false,
       msg: error,
-      code: "INTERNAL_ERROR",
-    })
+      code: 'INTERNAL_ERROR',
+    });
   }
-}
+};
 
 exports.logout = async (req, res, next) => {
   try {
     /*
         Simple destroy the cookie
     */
-    res.cookie("token", "", { httpOnly: true })
+    res.clearCookie('token');
     res.status(200).json({
-      msg: "Logged out successfully!",
+      msg: 'Logged out successfully!',
       success: true,
-      code: "LOGOUT_SUCCESS",
-    })
+      code: 'LOGOUT_SUCCESS',
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
       msg: error,
-      code: "INTERNAL_ERROR",
-    })
+      code: 'INTERNAL_ERROR',
+    });
   }
-}
+};
 
 const sendCookie = async (user, statusCode, msg, code, res) => {
   const token = await jwt.sign(
@@ -119,11 +119,11 @@ const sendCookie = async (user, statusCode, msg, code, res) => {
     {
       expiresIn: process.env.JWT_EXPIRY_TIME,
     }
-  )
-  res.cookie("token", token, {
+  );
+  res.cookie('token', token, {
     httpOnly: true,
     maxAge: 7 * 24 * 3600 * 1000,
-  })
+  });
   res.status(statusCode).json({
     success: true,
     msg,
@@ -131,5 +131,5 @@ const sendCookie = async (user, statusCode, msg, code, res) => {
     userID: user._id,
     username: user.username,
     email: user.email,
-  })
-}
+  });
+};
--- server.js
+++ server.js
@@ -1,69 +1,77 @@
-require("dotenv").config({ path: "./config.env" })
-const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
+require('dotenv').config({ path: './config.env' });
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-const connectDB = require("./config/db")
-const corsOptions = require("./utils/corsOptions")
-const app = express()
-connectDB()
-const { checkAuth } = require("./middlewares/checkAuth")
+const connectDB = require('./config/db');
+const corsOptions = require('./utils/corsOptions');
+const app = express();
+connectDB();
+const { checkAuth } = require('./middlewares/checkAuth');
 /**
  *   Enabled CORS on the website so you will not get
  *  errors on frontend
  */
-app.use(cors(corsOptions))
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 /*
   All auth routes are prefixed with /api/auth
   However you can replace this with anything as you
   wish
 */
-app.use("/api/auth", require("./routes/auth.route"))
+app.use('/api/auth', require('./routes/auth.route'));
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({
-    msg: "Hello from express auth",
+    msg: 'Hello from express auth',
     success: true,
-  })
-})
+  });
+});
 
-app.get("/secret", checkAuth, (req, res) => {
+app.get('/secret', checkAuth, (req, res) => {
   res.json({
-    msg: "Yay welcome dear user",
-  })
-})
+    msg: 'Hello mister',
+  });
+});
+
+app.get('/profile', checkAuth, (req, res) => {
+  const user = req.user;
+
+  res.json({
+    user: user,
+  });
+});
 
 /*
     This is a 404 catch all route, for those who are lost
 */
 app.use((req, res, next) => {
   res.status(404).json({
-    msg: "Requested resource was not found on this server",
+    msg: 'Requested resource was not found on this server',
     status: 404,
-  })
-  next()
-})
+  });
+  next();
+});
 
 app.listen(PORT, () => {
-  console.log("✅ Server : Started.")
-})
+  console.log('✅ Server : Started. on port: ' + PORT);
+});
 
 /**
  * This will output unhandled Rejection
  */
-process.on("unhandledRejection", (error, promise) => {
-  console.log(`❎Logged Error : ${error} \n ErrorStack : ${error.stack}`)
-})
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`❎Logged Error : ${error} \n ErrorStack : ${error.stack}`);
+});
 
 /**
  * This will output unhandled Execption
  */
-process.on("uncaughtException", function (error) {
-  console.log("❎ uncaughtException : ", error.stack)
-})
+process.on('uncaughtException', function (error) {
+  console.log('❎ uncaughtException : ', error.stack);
+});
