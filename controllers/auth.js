a14637fcfdb6385e854e805fb3b33e11a5fba11c
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-01-13 15:52:56 +0700

    fix : secure:true cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -127,6 +127,7 @@ const sendCookie = async (user, statusCode, msg, code, res) => {
     httpOnly: true,
     maxAge: 7 * 24 * 3600 * 1000,
     sameSite: 'none',
+    secure: true,
   });
   res.status(statusCode).json({
     success: true,
--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -5,7 +5,7 @@
  *
  * */
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://railway.app'],
   optionsSuccessStatus: 200,
   credentials: true,
 };
